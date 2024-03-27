6db15bb172ce7b3078b1690b5b58d2b9db6a1b5a
Jan Rozhon <jan@rozhon.eu>
2024-03-27 12:51:02 +0100

    04 - ami example call
    Signed-off-by: Jan Rozhon <jan@rozhon.eu>

--- 04-ami/scripts/originate.py
+++ 04-ami/scripts/originate.py
@@ -1,18 +1,50 @@
-from asterisk.ami import SimpleAction
-import sys
+import click
+from asterisk.ami import AMIClient, SimpleAction
 
-action = SimpleAction(
-    'Originate',
-    Channel='SIP/2010',
-    Exten=f'{}',
-    Priority=1,
-    Context='from-internal',
-    CallerID='python',
+ASTERISK_IP = "localhost"
+ASTERISK_PORT = 5038
+AMI_USERNAME = "amiuser"
+AMI_PASSWORD = "amipass"
+
+
+class AmiContextManager:
+    def __enter__(self):
+        self.client = AMIClient(address=ASTERISK_IP, port=ASTERISK_PORT)
+        self.client.login(username=AMI_USERNAME, secret=AMI_PASSWORD)
+        return self.client
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.client.logoff()
+
+
+@click.command()
+@click.option(
+    "-f",
+    "--from_ext",
+    default="10",
+    help="Extension that originates a call (can only be internal)",
 )
-client.send_action(action)
+@click.option(
+    "-t",
+    "--to_ext",
+    default="11",
+    help="Extension that receives the call (can be internal or external)",
+)
+def make_call(from_ext, to_ext):
+    with AmiContextManager() as client:
+        action = SimpleAction(
+            "Originate",
+            Channel=f"Local/{from_ext}@from-ami",
+            Exten=f"{to_ext}",
+            Priority=1,
+            Context="from-internal",
+            CallerID=f"{to_ext}",
+        )
+        client.send_action(action)
 
 
 def main():
+    make_call()
 
 
 if __name__ == "__main__":
